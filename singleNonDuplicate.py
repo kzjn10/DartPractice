1c6cc0f8ab46be1b87069f6ccb4a8f140d738ef3
“AnhNDT” <kzjn10@users.noreply.github.com>
2024-09-04 21:31:21 -0500

    improve code

--- singleNonDuplicate.py
+++ singleNonDuplicate.py
@@ -1,37 +1,30 @@
-class Solution():
+class Solution:
     def single_non_duplicate(self, nums: list) -> int:
         if len(nums) == 1:
             return nums[0]
         left = 0
         right = len(nums) - 1
-        temp=-1
-        while(left<=right):
-            mid = (left+right)//2
+        temp = -1
+        while left <= right:
+            mid = (left + right) // 2
             count_less_or_equal = 0
             for n in nums:
                 if n <= nums[mid]:
-                    count_less_or_equal+=1
+                    count_less_or_equal += 1
 
-            if(count_less_or_equal > 0 and
-               count_less_or_equal< len(nums)-1 and
-               nums[count_less_or_equal] != nums[count_less_or_equal-1] and
-               nums[count_less_or_equal] != nums[count_less_or_equal+1]):
-                return nums[count_less_or_equal]
-
-            if count_less_or_equal %2 != 0:
-                temp=mid
-                right=mid-1
+            if count_less_or_equal % 2 != 0:
+                temp = mid
+                right = mid - 1
             else:
                 left = mid + 1
 
         return nums[temp]
-    
+
+
 solution = Solution()
-print(solution.single_non_duplicate([0,1,1]))
-print(solution.single_non_duplicate([1,2,2,3,3]))
-print(solution.single_non_duplicate([1,1,2,2,3]))
-print(solution.single_non_duplicate([1,1,2,3,3,4,4,8,8]))
-print(solution.single_non_duplicate([3,3,7,7,10,11,11]))
-print(solution.single_non_duplicate([3,3,7,7,11,11,12]))
-
-
+print(solution.single_non_duplicate([0, 1, 1]))
+print(solution.single_non_duplicate([1, 2, 2, 3, 3]))
+print(solution.single_non_duplicate([1, 1, 2, 2, 3]))
+print(solution.single_non_duplicate([1, 1, 2, 3, 3, 4, 4, 8, 8]))
+print(solution.single_non_duplicate([3, 3, 7, 7, 10, 11, 11]))
+print(solution.single_non_duplicate([3, 3, 7, 7, 11, 11, 12]))
